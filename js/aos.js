f66a55af2d0c0957dde3cf924caeb83565730ddf
Thomas <tleclercq87@gmail.com>
2025-09-29 18:08:34 +0200

    optimisation finale

--- js/aos.js
+++ js/aos.js
@@ -1,10 +1,14 @@
-// js/aos.js
-window.addEventListener('load', () => {
-  if (window.matchMedia('(prefers-reduced-motion: reduce)').matches) return;
-
-  AOS.init({
-    once: true,
-    duration: 600,
-    offset: 80
-  });
+// Init AOS
+AOS.init({
+  once: true,
+  duration: 800
 });
+
+// Init GLightbox
+const lightbox = GLightbox({
+  selector: '.glightbox',
+  touchNavigation: true,
+  loop: true,
+  closeButton: true
+});
+
